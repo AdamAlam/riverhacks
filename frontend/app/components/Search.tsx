88f449a138b0a8cbdcca7335abea5c686ba845f6
Adam Alam <adamalam7@outlook.com>
2024-04-14 01:18:03 -0500

    Update Search component UI

--- frontend/app/components/Search.tsx
+++ frontend/app/components/Search.tsx
@@ -4,12 +4,18 @@ function Search({}: Props) {
     <div className="bg-search w-[100%] h-[30px] mt-2 border border-slate-600 flex items-center justify-center px-10 py-4">
       <p className="font-bold text-sm mr-2">Search For:</p>
       <input type="text" className="border border-black mr-2" />
-      <select name="cars" id="cars" className="border border-black mr-2">
-        {/* //TODO: Change these to the values on Yahoo! */}
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
+      <select
+        name="cars"
+        id="cars"
+        className="border border-black mr-2 text-md"
+      >
+        <option value="volvo">On the Web</option>
+        <option value="saab">In Images</option>
+        <option value="mercedes">In Video</option>
+        <option value="audi">In Directory</option>
+        <option value="audi">In Local</option>
+        <option value="audi">In News</option>
+        <option value="audi">In Shopping</option>
       </select>
       <button className="bg-[#efefef] border border-black text-xs p-1 mr-2 rounded-sm">
         Yahoo! Search
